297bdfd45af9a96d7ad8eadd6110461f3d366670
kavin1022 <pinghangfan1022@gmail.com>
2021-10-28 21:31:19 +1300

    added progess circle when logging in

--- Frontend/src/pages/SignIn.js
+++ Frontend/src/pages/SignIn.js
@@ -14,8 +14,12 @@
 	import { createTheme, ThemeProvider } from '@mui/material/styles';
 	import { useHistory } from 'react-router-dom';
 	import axios from "axios";
+	import CircularProgress from '@material-ui/core/CircularProgress';
+	import { useState } from 'react';
 
 	function Copyright(props) {
+
+		
 	return (
 		<Typography variant="body2" color="text.secondary" align="center" {...props}>
 		{'Copyright © '}
@@ -32,8 +36,10 @@
 
 	export default function SignIn() {
 		const history = useHistory();
+		const [isPending, setIsPending] = useState(false);
 
 		const handleSubmit = (event) => {
+			setIsPending(true);
 			event.preventDefault();
 			const data = new FormData(event.currentTarget);
 			// eslint-disable-next-line no-console
@@ -52,6 +58,7 @@
 				}else{
 					alert("An Error Occured")
 				}
+				setIsPending(false);
 			})
 			/*history.push("/")*/
 		};
@@ -60,64 +67,70 @@
 			<ThemeProvider theme={theme}>
 			<Container component="main" maxWidth="xs">
 				<CssBaseline />
-				<Box
-				sx={{
-					marginTop: 8,
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-				}}
-				>
-				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					Sign in
-				</Typography>
-				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-					<TextField
-					margin="normal"
-					required
-					fullWidth
-					id="email"
-					label="Username"
-					name="email"
-					autoComplete="email"
-					autoFocus
-					/>
-					<TextField
-					margin="normal"
-					required
-					fullWidth
-					name="password"
-					label="Password"
-					type="password"
-					id="password"
-					autoComplete="current-password"
-					/>
-					<Button
-					type="submit"
-					fullWidth
-					variant="contained"
-					sx={{ mt: 3, mb: 2 }}
-					>
-					Sign In
-					</Button>
-					<Grid container>
-					<Grid item xs>
-						<Link href="#" variant="body2">
-						Forgot password?
-						</Link>
-					</Grid>
-					<Grid item>
-						<Link href="/register" variant="body2">
-						{"Don't have an account? Sign Up"}
-						</Link>
-					</Grid>
-					</Grid>
-				</Box>
+				
+				<Box sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+				}}>
+					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+						<LockOutlinedIcon />
+						
+					</Avatar>
+					
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					{isPending && <CircularProgress/>}
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Username"
+						name="email"
+						autoComplete="email"
+						autoFocus
+						/>
+						<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+						/>
+						<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+						>
+						Sign In
+						</Button>
+						
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+								Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href="/register" variant="body2">
+								{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+						
+					</Box>
 				</Box>
 			</Container>
+			
 			</ThemeProvider>
+			
 		);
 	}
